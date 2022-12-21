9af08e255a46283b151ac3a60fccfa795f052507
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2022-12-21 15:29:06 +0100

    Add event variable for database

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const BOX_ID = "81b280352bdf076b4390";
 const API_KEY = "d3f80f81-6910-4d0d-933c-a53eb7c53fc5";
-const BASE_URL = `http://localhost:3001/box_${BOX_ID}`;
+const DB_URL = process.env.REACT_APP_DB_URL;
+const BASE_URL = `${DB_URL}/box_${BOX_ID}`;
 
 export const createAxiosInstance = () =>
   axios.create({
