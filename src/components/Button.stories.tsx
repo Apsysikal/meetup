d5dd4c1db9fdc70bf42ad5214e0efb2f13c3f790
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2022-12-22 17:25:33 +0100

    Add Storybook

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,13 @@
+// Button.stories.ts|tsx
+
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+export default {
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+export const Primary: ComponentStory<typeof Button> = () => <Button>Button</Button>;
