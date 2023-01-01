7167bd2af00edee43e642c7c68262d54cbc4c7b9
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2023-01-01 22:08:41 +0100

    Fix empty page

--- app/routes/event/index.tsx
+++ app/routes/event/index.tsx
@@ -1,10 +1,22 @@
-import { Link } from "@remix-run/react";
+import { useNavigate } from "@remix-run/react";
+
+import { Button } from "~/components/Button";
 
 export default function EventIndexRoute() {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <p>Please use a link to get to an existing event or create a new one</p>
-      <Link to="new">New Event</Link>
-    </>
+    <div className="flex h-full">
+      <div className="my-auto flex flex-col gap-2 items-center">
+        <p className="text-slate-700 text-center">
+          Please use a link to get to an existing event or create a new one.
+        </p>
+        <div className="shrink">
+          <Button onClick={() => navigate("/event/new")}>
+            <p>Plan an Event</p>
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
